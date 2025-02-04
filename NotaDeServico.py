438df163f406d2613a984d50485f5f291fea250c
Felipe Belloni <141536766+yBelloni@users.noreply.github.com>
2025-02-04 15:15:00 -0300

    Update NotaDeServico.py

--- NotaDeServico.py
+++ NotaDeServico.py
@@ -15,12 +15,32 @@ from dados import *
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-def life():
-    licenca_expiracao = datetime(2025, 7, 15)
-    if datetime.now() > licenca_expiracao:
+def efil():
+    exp = datetime(2025, 7, 15)
+    if datetime.now() > exp:
         exit()
     else:
         time.sleep(2)
+def escolhaContrato(opcao):
+        contratos_dados = {
+        1: {"diretorio": diretorio0916, "emailGestor": emailGestor0916},
+        2: {"diretorio": diretorio1168, "emailGestor": emailGestor1168},
+        3: {"diretorio": diretorio1169, "emailGestor": emailGestor1169},
+        4: {"diretorio": diretorio1170, "emailGestor": emailGestor1170},
+        5: {"diretorio": diretorio1203, "emailGestor": emailGestor1203},
+        6: {"diretorio": diretorio1315, "emailGestor": emailGestor1315},
+        7: {"diretorio": diretorio1388, "emailGestor": emailGestor1388},
+        8: {"diretorio": diretorio1395, "emailGestor": emailGestor1395},
+        9: {"diretorio": diretorio1433, "emailGestor": emailGestor1433},
+        10: {"diretorio": diretorio1446, "emailGestor": emailGestor1446},
+        11: {"diretorio": diretorio1470, "emailGestor": emailGestor1470}}
+        if opcao in contratos_dados:
+            contrato = contratos_dados[opcao]
+            print(f"\nIniciando Contrato {contratos[opcao]}")
+            return contrato["diretorio"], contrato["emailGestor"]
+        else:
+            print("\nOpção inválida. Digite um número de 1 a 11.")
+            return None, None     
 def menu():
     while True:
         print("\nDigite um número de 1 a 11 para qual contrato inicializar:")
@@ -30,66 +50,17 @@ def menu():
         try:
             opcao = int(opcao)
             if 1 <= opcao <= 11:
-                break
+                diretorio, emailGestor = escolhaContrato(opcao)
+                if diretorio and emailGestor:
+                    # print(f"\nDiretório: {diretorio}, Email Gestor: {emailGestor}")
+                    break
             else:
                 print("\nOpção inválida. Digite um número entre 1 e 11.")
         except ValueError:
-            print("Entrada inválida. Por favor, digite um número inteiro.")
-def escolhaContrato(opcao):
-        match int(opcao):
-            case 1:
-            #rio piracicaba
-                print("\nIniciando Contrato 30000916")
-                diretorio = diretorio0916
-                emailGestor = emailGestor916
-            case 2:
-            #sao goncalo rio abaixo
-                print("\nIniciando Contrato 30001168")
-                diretorio = diretorio1168
-                emailGestor = emailGestor1168
-            case 3:
-                print("\nIniciando Contrato 30001169")
-                diretorio = diretorio1169
-                emailGestor = emailGestor1169
-            case 4:
-                print("\nIniciando Contrato 30001170")
-                diretorio = diretorio1170
-                emailGestor = emailGestor1170
-            case 5:
-                print("\nIniciando Contrato 30001203")
-                diretorio = diretorio1203
-                emailGestor = emailGestor1203
-            case 6:
-                print("\nIniciando Contrato 30001315")
-                diretorio = diretorio1315
-                emailGestor = emailGestor1315
-            case 7:
-                print("\nIniciando Contrato 30001388")
-                diretorio = diretorio1388
-                emailGestor = emailGestor1388
-            case 8:
-                print("\nIniciando Contrato 30001395")
-                diretorio = diretorio1395
-                emailGestor = emailGestor1395
-            case 9:
-                print("\nIniciando Contrato 30001433")
-                diretorio = diretorio1433
-                emailGestor = emailGestor1433
-            case 10:
-                print("\nIniciando Contrato 30001446")
-                diretorio = diretorio1446
-                emailGestor = emailGestor1446
-            case 11:
-                print("\nIniciando Contrato 30001470")
-                diretorio = diretorio1470
-                emailGestor = emailGestor1470
-            case _:
-                print("\nOpção inválida. Digite um numero de 1 a 8")
-            
+            print("\nEntrada inválida. Por favor, digite um número inteiro.")
 def Login():
     link = "https://vale.virtual360.io/"
     nav.get(link)
-    nav.maximize_window()
     btnStart = nav.find_element(By.ID, "login_portal")
     btnStart.click()
     inputEmail = nav.find_element(By.ID, "user_login")
@@ -101,17 +72,34 @@ def Login():
 def JoinNotaFiscalDeServicos():
     nav.find_element(By.TAG_NAME, "body").send_keys(Keys.CONTROL + 't')
     nav.get("https://vale.virtual360.io/nf/tax_documents/service_invoice/new")
+def BuscarArquivos(archive_num):
+    arquivoEncontrado = []
+    if not os.path.exists(diretorio):
+        raise FileNotFoundError(f"Arquivo não encontrado")
+    for arquivo in os.listdir(diretorio):
+        if archive_num in arquivo:
+            arquivoEncontrado.append(arquivo)
+    if len(arquivoEncontrado) == 2:
+        for arquivo in arquivoEncontrado:
+            caminho_completo = os.path.join(diretorio, arquivo)
+            if arquivo.endswith('xml'):
+                xmlCaminho = caminho_completo
+            elif arquivo.endswith('pdf'):
+                pdfCaminho = caminho_completo
+        return xmlCaminho, pdfCaminho
+    else:
+        raise FileNotFoundError("Não há arquivos suficientes com esse nome no diretório.")
 def InsertArchives(xmlCaminho, pdfCaminho):
     xmlUp = WebDriverWait(nav, 10).until(
         EC.presence_of_element_located((By.ID, "tax_document_document_xml"))
     )
     xmlUp.send_keys(xmlCaminho)
-    time.sleep(7)
+    time.sleep(10)
     pdfUp = WebDriverWait(nav, 10).until(
         EC.presence_of_element_located((By.ID, "tax_document_document_pdf"))
     )
     pdfUp.send_keys(pdfCaminho)
-    time.sleep(10)
+    time.sleep(40)
     inputGestor = WebDriverWait(nav, 10).until(
         EC.presence_of_element_located((By.ID, "tax_document_requester_area"))
     )
@@ -130,23 +118,6 @@ def SubmitNota():
         time.sleep(3)
         exit()
     time.sleep(1)
-def BuscarArquivos(archive_num):
-    arquivoEncontrado = []
-    if not os.path.exists(diretorio):
-        raise FileNotFoundError(f"Arquivo não encontrado")
-    for arquivo in os.listdir(diretorio):
-        if archive_num in arquivo:
-            arquivoEncontrado.append(arquivo)
-    if len(arquivoEncontrado) == 2:
-        for arquivo in arquivoEncontrado:
-            caminho_completo = os.path.join(diretorio, arquivo)
-            if arquivo.endswith('xml'):
-                xmlCaminho = caminho_completo
-            elif arquivo.endswith('pdf'):
-                pdfCaminho = caminho_completo
-        return xmlCaminho, pdfCaminho
-    else:
-        raise FileNotFoundError("Não há arquivos suficientes com esse nome no diretório.")
 def DadosExcel():
     IndexProtocolo = nav.find_element(By.CLASS_NAME, 'v-h4')
     ProtocoloCompleto = IndexProtocolo.text
@@ -161,7 +132,7 @@ def DadosExcel():
     print(f'\nnumero Nota: {NumeroNota}')
     time.sleep(2)
     try:
-        wb = load_workbook('relatorio.xlsx')
+        wb = load_workbook('relatorio2.xlsx')
         ws = wb.active
     except Exception as e:
         print(f'Ocorreu um erro ao inicializar a planilha {e}') 
@@ -172,6 +143,7 @@ def DadosExcel():
         try:
             if ws[celulaP].value is None:
                 ws[celulaP].value = NumeroProtocolo
+                wb.save('relatorio.xlsx')
                 print('Protocolo Computado')
                 break
             else:
@@ -185,34 +157,30 @@ def DadosExcel():
         try:
             if ws[celulaN].value is None:
                 ws[celulaN].value = NumeroNota
+                wb.save('relatorio.xlsx')
                 print('Nota Computada')
                 break
             else:
                 print('Esta celula já contém dados. Tente novamente.')
         except KeyError:
             print("Célula inválida. Por favor, digite uma célula válida.")
-    wb.save('relatorio.xlsx')
 if __name__ == "__main__":
     try:
-        life()
+        efil()
         menu()
-        contratoEscolhido = input("\n")
-        escolhaContrato(contratoEscolhido)
         nav = webdriver.Chrome()
         Login()
         while True:
             print('\nNúmero do Arquivo: ')
+            archive_num = input()
             try:
-                archive_num = input().strip()
-                archive_num = int(archive_num)
-            except ValueError:
-                print("\nDigite um número válido")
-            try:
-                xmlCaminho, pdfCaminho = BuscarArquivos(archive_num)
                 JoinNotaFiscalDeServicos()
+                xmlCaminho, pdfCaminho = BuscarArquivos(archive_num)
+                time.sleep(1)
                 InsertArchives(xmlCaminho, pdfCaminho)
                 SubmitNota()
                 print("\nFormulário enviado com sucesso!")
+                time.sleep(5)
                 DadosExcel()
                 print('\nDados Computados no Excel')
                 time.sleep(2)
@@ -226,4 +194,4 @@ if __name__ == "__main__":
     except Exception as e:
         print(f'ERRO {e}')
     finally:
-        nav.quit()
+        exit()
